fe2dca158969d0d1738144f210d827e25f317532
tealjulia <teal@airbyte.io>
2022-05-06 16:09:58 -0400

    move documentation url to context

--- airbyte-webapp/src/pages/SourcesPage/pages/CreateSourcePage/components/SourceForm.tsx
+++ airbyte-webapp/src/pages/SourcesPage/pages/CreateSourcePage/components/SourceForm.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormattedMessage } from "react-intl";
 
 import { ConnectionConfiguration } from "core/domain/connection";
-import { SourceDefinition, SourceDefinitionSpecification } from "core/domain/connector";
+import { SourceDefinition } from "core/domain/connector";
 import { LogsRequestError } from "core/request/LogsRequestError";
+import useRouter from "hooks/useRouter";
 import { TrackActionType, useTrackAction } from "hooks/useTrackAction";
+import { useGetSourceDefinitionSpecificationAsync } from "services/connector/SourceDefinitionSpecificationService";
 import { createFormErrorMessage } from "utils/errorStatusMessage";
 import { ConnectorCard } from "views/Connector/ConnectorCard";
+import { useDocumentationPanelContext } from "views/Connector/ConnectorDocumentationLayout/ConnectorDocumentationContext";
 import { ServiceFormValues } from "views/Connector/ServiceForm/types";
 
-interface SourceFormProps {
+type SourceFormProps = {
   onSubmit: (values: {
     name: string;
     serviceType: string;
@@ -20,29 +23,42 @@ interface SourceFormProps {
   sourceDefinitions: SourceDefinition[];
   hasSuccess?: boolean;
   error?: { message?: string; status?: number } | null;
-  setSourceDefinitionId: React.Dispatch<React.SetStateAction<string | null>> | null;
-  sourceDefinitionSpecification: SourceDefinitionSpecification | undefined;
-  sourceDefinitionError: Error | null;
-  isLoading: boolean;
-}
+};
 
-const SourceForm: React.FC<SourceFormProps> = ({
+const hasSourceDefinitionId = (state: unknown): state is { sourceDefinitionId: string } => {
+  return (
+    typeof state === "object" &&
+    state !== null &&
+    typeof (state as { sourceDefinitionId?: string }).sourceDefinitionId === "string"
+  );
+};
+
+export const SourceForm: React.FC<SourceFormProps> = ({
   onSubmit,
   sourceDefinitions,
   error,
   hasSuccess,
   afterSelectConnector,
-  setSourceDefinitionId,
-  sourceDefinitionSpecification,
-  sourceDefinitionError,
-  isLoading,
 }) => {
+  const { location } = useRouter();
+  const { setDocumentationUrl, setDocumentationPanelOpen } = useDocumentationPanelContext();
   const trackNewSourceAction = useTrackAction(TrackActionType.NEW_SOURCE);
 
-  //TODO: today's changes broke some functionality on the source creation form within the connectors setup flow if users are completing it there...
+  const [sourceDefinitionId, setSourceDefinitionId] = useState<string | null>(
+    hasSourceDefinitionId(location.state) ? location.state.sourceDefinitionId : null
+  );
+
+  const {
+    data: sourceDefinitionSpecification,
+    error: sourceDefinitionError,
+    isLoading,
+  } = useGetSourceDefinitionSpecificationAsync(sourceDefinitionId);
+
   const onDropDownSelect = (sourceDefinitionId: string) => {
-    setSourceDefinitionId && setSourceDefinitionId(sourceDefinitionId);
+    setDocumentationPanelOpen(false);
+    setSourceDefinitionId(sourceDefinitionId);
     const connector = sourceDefinitions.find((item) => item.sourceDefinitionId === sourceDefinitionId);
+    setDocumentationUrl(connector?.documentationUrl || "");
 
     if (afterSelectConnector) {
       afterSelectConnector();
@@ -74,15 +90,9 @@ const SourceForm: React.FC<SourceFormProps> = ({
       fetchingConnectorError={sourceDefinitionError}
       errorMessage={errorMessage}
       isLoading={isLoading}
-      formValues={
-        sourceDefinitionSpecification
-          ? { serviceType: sourceDefinitionSpecification.sourceDefinitionId, name: "" }
-          : undefined
-      }
+      formValues={sourceDefinitionId ? { serviceType: sourceDefinitionId, name: "" } : undefined}
       title={<FormattedMessage id="onboarding.sourceSetUp" />}
       jobInfo={LogsRequestError.extractJobInfo(error)}
     />
   );
 };
-
-export default SourceForm;
