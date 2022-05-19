83121b4d2d15b5bd8b2a70fd977d702f4d2a82ab
Sunil Sadasivan <sunils34+sunil-sadasivan@gmail.com>
2022-05-19 08:39:24 -0400

    Initial check in of senate lobbyist connector

--- airbyte-integrations/connectors/source-senate-lobbyist/source_senate_lobbyist/source.py
+++ airbyte-integrations/connectors/source-senate-lobbyist/source_senate_lobbyist/source.py
@@ -10,7 +10,7 @@ import requests
 from airbyte_cdk.sources import AbstractSource
 from airbyte_cdk.sources.streams import Stream
 from airbyte_cdk.sources.streams.http import HttpStream
-from airbyte_cdk.sources.streams.http.auth import TokenAuthenticator
+from airbyte_cdk.sources.streams.http.auth import HttpAuthenticator
 
 """
 TODO: Most comments in this class are instructive and should be deleted after the source is implemented.
@@ -56,7 +56,11 @@ class SenateLobbyistStream(HttpStream, ABC):
     """
 
     # TODO: Fill in the url base. Required.
-    url_base = "https://example-api.com/v1/"
+    url_base = "https://lda.senate.gov/api/v1/"
+
+    def __init__(self, config: Mapping[str, Any]):
+        super().__init__(authenticator=config["authenticator"])
+        self.config = config
 
     def next_page_token(self, response: requests.Response) -> Optional[Mapping[str, Any]]:
         """
@@ -73,7 +77,11 @@ class SenateLobbyistStream(HttpStream, ABC):
         :return If there is another page in the result, a mapping (e.g: dict) containing information needed to query the next page in the response.
                 If there are no more pages in the result, return None.
         """
-        return None
+        r = response.json()
+        if r['next'] is None:
+            return None
+        page = int(r['next'].split('=')[1])
+        return { 'nextUrl': r['next'] }
 
     def request_params(
         self, stream_state: Mapping[str, Any], stream_slice: Mapping[str, any] = None, next_page_token: Mapping[str, Any] = None
@@ -82,23 +90,32 @@ class SenateLobbyistStream(HttpStream, ABC):
         TODO: Override this method to define any query parameters to be set. Remove this method if you don't need to define request params.
         Usually contains common params e.g. pagination size etc.
         """
-        return {}
+        page = 1
+        if (next_page_token is not None and next_page_token['nextUrl']):
+            page = int(next_page_token['nextUrl'].split('=')[1])
+        return {'page': page}
 
     def parse_response(self, response: requests.Response, **kwargs) -> Iterable[Mapping]:
         """
         TODO: Override this method to define how a response is parsed.
         :return an iterable containing each record in the response
         """
-        yield {}
 
+        json_response = response.json()
+        for record in json_response["results"]:
+            r = record
+            r['registrant_id'] = r['registrant']['id']
+            r['registrant_name'] = r['registrant']['name']
+            r['registrant_description'] = r['registrant']['description']
+            r['registrant_contact_name'] = r['registrant']['contact_name']
+            r['registrant_telephone'] = r['registrant']['contact_telephone']
+            r['registrant_updated_at'] = r['registrant']['dt_updated']
+            yield r
 
-class Customers(SenateLobbyistStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
+class Lobbyists(SenateLobbyistStream):
 
     # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "customer_id"
+    primary_key = "id"
 
     def path(
         self, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None
@@ -107,81 +124,33 @@ class Customers(SenateLobbyistStream):
         TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/customers then this
         should return "customers". Required.
         """
-        return "customers"
+        return "lobbyists"
 
 
-# Basic incremental stream
-class IncrementalSenateLobbyistStream(SenateLobbyistStream, ABC):
+class SenateAuthenticator(HttpAuthenticator):
     """
-    TODO fill in details of this class to implement functionality related to incremental syncs for your connector.
-         if you do not need to implement incremental sync for any streams, remove this class.
+    Generate auth header for start making requests from API token and API key.
     """
 
-    # TODO: Fill in to checkpoint stream reads after N records. This prevents re-reading of data if the stream fails for any reason.
-    state_checkpoint_interval = None
+    def __init__(
+        self,
+        key: str,
+        auth_header: str = "Authorization",
+    ):
+        self.auth_header = auth_header
+        self._key = key
 
-    @property
-    def cursor_field(self) -> str:
-        """
-        TODO
-        Override to return the cursor field used by this stream e.g: an API entity might always use created_at as the cursor field. This is
-        usually id or date based. This field's presence tells the framework this in an incremental stream. Required for incremental.
-
-        :return str: The name of the cursor field.
-        """
-        return []
-
-    def get_updated_state(self, current_stream_state: MutableMapping[str, Any], latest_record: Mapping[str, Any]) -> Mapping[str, Any]:
-        """
-        Override to determine the latest state after reading the latest record. This typically compared the cursor_field from the latest record and
-        the current state and picks the 'most' recent cursor. This is how a stream's state is determined. Required for incremental.
-        """
-        return {}
-
-
-class Employees(IncrementalSenateLobbyistStream):
-    """
-    TODO: Change class name to match the table/data source this stream corresponds to.
-    """
-
-    # TODO: Fill in the cursor_field. Required.
-    cursor_field = "start_date"
-
-    # TODO: Fill in the primary key. Required. This is usually a unique field in the stream, like an ID or a timestamp.
-    primary_key = "employee_id"
-
-    def path(self, **kwargs) -> str:
-        """
-        TODO: Override this method to define the path this stream corresponds to. E.g. if the url is https://example-api.com/v1/employees then this should
-        return "single". Required.
-        """
-        return "employees"
-
-    def stream_slices(self, stream_state: Mapping[str, Any] = None, **kwargs) -> Iterable[Optional[Mapping[str, any]]]:
-        """
-        TODO: Optionally override this method to define this stream's slices. If slicing is not needed, delete this method.
-
-        Slices control when state is saved. Specifically, state is saved after a slice has been fully read.
-        This is useful if the API offers reads by groups or filters, and can be paired with the state object to make reads efficient. See the "concepts"
-        section of the docs for more information.
-
-        The function is called before reading any records in a stream. It returns an Iterable of dicts, each containing the
-        necessary data to craft a request for a slice. The stream state is usually referenced to determine what slices need to be created.
-        This means that data in a slice is usually closely related to a stream's cursor_field and stream_state.
-
-        An HTTP request is made for each returned slice. The same slice can be accessed in the path, request_params and request_header functions to help
-        craft that specific request.
-
-        For example, if https://example-api.com/v1/employees offers a date query params that returns data for that particular day, one way to implement
-        this would be to consult the stream state object for the last synced date, then return a slice containing each date from the last synced date
-        till now. The request_params function would then grab the date from the stream_slice and make it part of the request by injecting it into
-        the date query param.
-        """
-        raise NotImplementedError("Implement stream slices or delete this method!")
-
+    def get_auth_header(self) -> Mapping[str, Any]:
+        return {self.auth_header: f'Token {self._key}'}
 
 # Source
 class SourceSenateLobbyist(AbstractSource):
+
+    @staticmethod
+    def _get_authenticator(config: dict) -> SenateAuthenticator:
+        key = config["apiKey"]
+        return SenateAuthenticator(key=key)
+
     def check_connection(self, logger, config) -> Tuple[bool, any]:
         """
         TODO: Implement a connection check to validate that the user-provided config can be used to connect to the underlying API
@@ -193,7 +162,15 @@ class SourceSenateLobbyist(AbstractSource):
         :param logger:  logger object
         :return Tuple[bool, any]: (True, None) if the input config can be used to connect to the API successfully, (False, error) otherwise.
         """
-        return True, None
+        try:
+            authenticator = self._get_authenticator(config)
+            url = f"{SenateLobbyistStream.url_base}"
+
+            response = requests.get(url, headers=authenticator.get_auth_header())
+            response.raise_for_status()
+            return True, None
+        except requests.exceptions.RequestException as e:
+            return False, e
 
     def streams(self, config: Mapping[str, Any]) -> List[Stream]:
         """
@@ -201,6 +178,5 @@ class SourceSenateLobbyist(AbstractSource):
 
         :param config: A Mapping of the user input configuration as defined in the connector spec.
         """
-        # TODO remove the authenticator if not required.
-        auth = TokenAuthenticator(token="api_key")  # Oauth2Authenticator is also available if you need oauth support
-        return [Customers(authenticator=auth), Employees(authenticator=auth)]
+        config["authenticator"] = self._get_authenticator(config)
+        return [Lobbyists(config)]
